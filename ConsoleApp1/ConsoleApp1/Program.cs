d039f045a0e0cc8210e5d7103d1f31e245520012
Mattias Eklund <mattias.eklund@ockelbook.se>
2026-02-02 10:47:35 +0100

    First commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using ConsoleApp1;
+using System;
+using System.Reflection;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args)    
     {
-        
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 200);
+        Book book2 = new Book("Hello", "Adele", 250);
+        //book1.title = "The Great Gatsby";
+        //book1.author = "F. Scott Fitzgerald";
+        //book1.pages = 180;
+
+        Console.WriteLine(Book.bookCount);
+        Console.WriteLine(book2.author);
 
         Console.ReadLine();        
-    }
-
+    }    
+    
 }
--- ConsoleApp1/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f05aabb093c06c0aefa9d0391d48d21e2798ee7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
