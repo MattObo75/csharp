c743d025a76c13238c5800f8a6442a5829a5a4a1
Mattias Eklund <mattias.eklund@ockelbook.se>
2026-02-10 15:05:34 +0100

    Uppdaterar Dag2-Klassövning 2

--- Labbar/Vecka 7/Dag2/Klasser2/Klasser2/Program.cs
+++ Labbar/Vecka 7/Dag2/Klasser2/Klasser2/Program.cs
@@ -6,11 +6,13 @@
         {
             Wizard gandalf = new Wizard { Name = "Gandalf", Mana = 100 };
             Wizard merlin = new Wizard { Name = "Merlin", Mana = 80 };
+
             // Gandalf kastar en spell
             int usedMana = gandalf.CastSpell(30);
 
             // Merlin får manan Gandalf använde
             merlin.ReceiveMana(usedMana);
+
             // Merlin kastar två spells
             merlin.CastSpell(20);
             merlin.CastSpell(10);
@@ -32,21 +34,30 @@
             public static int TotalSpellsCast = 0;
             public int CastSpell(int cost)
             {
+                if (Mana < cost)
+                {
+                    Console.WriteLine($"{Name} har inte tillräckligt med mana.");
+                    return 0;
+                }
+                
                 Mana -= cost;
                 TotalSpellsCast++;
-                return Mana;
+
+                Console.WriteLine($"{Name} kastar en spell och använder {cost} mana.");
+                return cost;
             }
 
             public void ReceiveMana(int amount)
             {
                 Mana += amount;
+                Console.WriteLine($"{Name} får {amount} mana.");
             }
 
             public static void ShowGlobalStats()
             {
-                   // Totalt antal kastade trollformler (statisk)
+                // Totalt antal kastade trollformler (statisk)
                 Console.WriteLine();
-                Console.WriteLine($"Total number of spells casted: {TotalSpellsCast}");
+                Console.WriteLine($"Totalt antal kastade spells: {TotalSpellsCast}");
             }
 
         }
